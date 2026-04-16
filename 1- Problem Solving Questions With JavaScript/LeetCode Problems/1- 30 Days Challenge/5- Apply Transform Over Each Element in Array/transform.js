5e9d6113798889a6068ab0936152db8d1f435d9e
AliMahmoud26 <alimah.engineer@gmail.com>
2026-04-16 13:49:17 +0200

    Uploading the solution of Apply Transform over Each Element in Array Question - 30 Days of JavaScript Challenges

---
+++ 1- Problem Solving Questions With JavaScript/LeetCode Problems/1- 30 Days Challenge/5- Apply Transform Over Each Element in Array/transform.js
@@ -0,0 +1,85 @@
+/* 
+2635. Apply Transform Over Each Element in Array
+Solved
+Easy
+Companies
+Hint
+Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+Example 1:
+
+Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+Output: [2,3,4]
+Explanation:
+const newArray = map(arr, plusone); // [2,3,4]
+The function increases each value in the array by one. 
+*/
+
+// The Solution
+// const map = function(arr, fn) {
+//     let newArray = []
+//     for (let i = 0; i < arr.length; i++) {
+//         newArray.push(fn(arr[i], i))
+//     }
+//     return newArray
+// }
+
+// let arr = [1, 2, 3];
+// let plusone = function(n) { return n + 1; };
+// let result = map(arr, plusone);
+// console.log(result);
+
+// First approach using for loop
+let map = function(arr, fn) {
+    let newArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(fn(arr[i], i));
+    }
+    
+    return newArray;
+}
+
+// Second approach using for...of loop
+let ofMap = function(arr, fn) {
+    let newArray = [];
+    let index = 0;
+
+    for (const element of arr) {
+        newArray.push(fn(element, index));
+        index++;
+    }
+
+    return newArray;
+}
+
+// Third approach using for...each loop
+let eachMap = function(arr, fn) {
+    let newArray = [];
+
+    arr.forEach((element, index) => {
+        newArray.push(fn(element, index));
+    });
+
+    return newArray;
+}
+
+// Map function calls
+let arr = [1, 2, 3]
+let plusone = function(n) {return n + 1}
+let result = map(arr, plusone)
+console.log(result);
+
+// For...of loop map function call
+let ofArr = [10, 20, 30]
+let ofResult = ofMap(ofArr, plusone)
+console.log(ofResult);
+
+// For...each loop map function call
+let eachArr = [100, 200, 300]
+let eachResult = eachMap(eachArr, plusone)
+console.log(eachResult);
